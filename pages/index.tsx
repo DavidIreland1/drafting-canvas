85ac94f4cbb5dde04edddab496fcb1d84caa8e7b
David Ireland <david.ireland@accenture.com>
2021-09-24 10:37:18 +1000

    deleted index

--- pages/index.tsx
+++
@@ -1,8 +0,0 @@
-import { generateID } from '../utils/utils';
-
-export default function OffWhiteCanvas(): JSX.Element {
-	function redirect() {
-		window.location.href += generateID(20);
-	}
-	return <div onClick={redirect}>Redirect</div>;
-}
