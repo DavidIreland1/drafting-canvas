5a2acbc3136d0e83a576691e114acf8f8c13ed26
David Ireland <david.ireland@accenture.com>
2021-11-04 04:14:27 +1100

    added link from main page

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,40 +1,43 @@
-import Head from 'next/head'
+import Head from 'next/head';
+
+import { useRouter } from 'next/router';
 
 export default function Home(): JSX.Element {
-  return (
-    <div className="container">
-      <Head>
-        <title>Create Next App</title>
-        <link rel="icon" href="/favicon.ico" />
-      </Head>
+	const router = useRouter();
 
-      <main>
-		  
-        <h1>Hello</h1>
-      </main>
+	return (
+		<div className="container">
+			<Head>
+				<title>Create Next App</title>
+				<link rel="icon" href="/favicon.ico" />
+			</Head>
 
-      <style jsx>{`
-        canvas {
-          width: 100vw;
-          height: 100vh;
-		  background: #F1F1F1;
-        }
-      `}</style>
+			<main>
+				<h1>Hello</h1>
 
-      <style jsx global>{`
-        html,
-        body {
-          padding: 0;
-          margin: 0;
-          font-family: -apple-system, BlinkMacSystemFont, Segoe UI, Roboto,
-            Oxygen, Ubuntu, Cantarell, Fira Sans, Droid Sans, Helvetica Neue,
-            sans-serif;
-        }
+				<button onClick={() => router.push('111')}>Create New Page</button>
+			</main>
 
-        * {
-          box-sizing: border-box;
-        }
-      `}</style>
-    </div>
-  )
+			<style jsx>{`
+				canvas {
+					width: 100vw;
+					height: 100vh;
+					background: #f1f1f1;
+				}
+			`}</style>
+
+			<style jsx global>{`
+				html,
+				body {
+					padding: 0;
+					margin: 0;
+					font-family: -apple-system, BlinkMacSystemFont, Segoe UI, Roboto, Oxygen, Ubuntu, Cantarell, Fira Sans, Droid Sans, Helvetica Neue, sans-serif;
+				}
+
+				* {
+					box-sizing: border-box;
+				}
+			`}</style>
+		</div>
+	);
 }
